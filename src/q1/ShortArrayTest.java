e0591b53741dfc0c9c1c08e5c0cfea86a5208c3d
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-06 21:13:21 -0300

    202201062113 Some questions of Java 8 simulated test

---
+++ src/q1/ShortArrayTest.java
@@ -0,0 +1,10 @@
+package q1;
+
+public class ShortArrayTest {
+    public static void main(String[] args){
+        short[] arr = new short[]{50,50};
+        arr[0] = 5;
+        arr[1] = 10;
+        System.out.println(arr[0]+","+arr[1]);
+    }
+}
---
+++ src/q1/Student.java
@@ -0,0 +1,11 @@
+package q1;
+
+class Student {
+    String name;
+    int marks;
+
+    Student (String name, int marks){
+        this.name = name;
+        this.marks = marks;
+    }
+}
