c6674462fa97b5678af787118e8f3719e46224b2
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-07 20:13:23 -0300

    202201072013 Some questions of Java 8 simulated test

---
+++ src/q1/Animal.java
@@ -0,0 +1,14 @@
+package q1;
+
+abstract class Animal {
+    private String name;
+
+    Animal(String n) {
+        this.name = n;
+    }
+
+    public String getName(){
+        return name;
+    }
+}
+
