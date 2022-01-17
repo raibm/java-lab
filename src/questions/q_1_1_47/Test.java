ae0657823e34086a7dbaf6de1e2ab6a3c3d385d4
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-17 20:35:04 -0300

    202201172034 More questions...

---
+++ src/questions/q_1_1_47/Test.java
@@ -0,0 +1,11 @@
+package questions.q_1_1_47;
+
+public class Test {
+
+    public static void main(String[] args) {
+        String a = "ABCD";
+        String b = "ABCD"+"";
+
+        System.out.println(a == b);
+    }
+}
---
+++ src/questions/q_1_1_48/Test.java
@@ -0,0 +1,12 @@
+package questions.q_1_1_48;
+
+public class Test {
+
+    public static void main(String[] args) {
+       String bankCode = "ICICINBBRT4";
+
+       String sub = bankCode.substring(4, 6);
+
+       System.out.println(sub);
+    }
+}
