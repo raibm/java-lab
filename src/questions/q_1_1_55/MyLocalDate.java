aeffe3218f7b9fb31f7adc2b4890d6869aa089c4
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-19 20:10:58 -0300

    202201192010 Easy, easy, easy

---
+++ src/questions/q_1_1_55/MyLocalDate.java
@@ -0,0 +1,13 @@
+package questions.q_1_1_55;
+
+import java.time.LocalDate;
+
+//public class MyLocalDate extends LocalDate {
+
+// da td errado, joia
+
+//    @Override
+//    public String toString(){
+//        return super.getDayOfMonth()+"";
+//    }
+//}
