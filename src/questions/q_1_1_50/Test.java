6c52b0357cf53d63ae558647601cf5060c551ece
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-18 21:20:09 -0300

    202201182119 Stringbuilder curious question

---
+++ src/questions/q_1_1_50/Test.java
@@ -0,0 +1,11 @@
+package questions.q_1_1_50;
+
+public class Test {
+
+    public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder(100);
+
+        System.out.println(sb.length()+":"+sb.toString().length());
+        // é falso o_o
+    }
+}
