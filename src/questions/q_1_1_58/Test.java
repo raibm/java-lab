d923550dbdf5476e9e3265a2f3900a3fdc172899
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-20 16:49:49 -0300

    202201201649 LocalDateTime question

---
+++ src/questions/q_1_1_58/Test.java
@@ -0,0 +1,13 @@
+package questions.q_1_1_58;
+
+import java.time.LocalDateTime;
+
+public class Test {
+
+    public static void main(String[] args) {
+    LocalDateTime obj = LocalDateTime.now();
+
+    System.out.println(obj.getSecond());
+
+    }
+}
