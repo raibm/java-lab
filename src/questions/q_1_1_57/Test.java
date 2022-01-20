48ae379b76c9b8ab6fdda4ccbcf91a111bc6859b
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-20 16:31:18 -0300

    202201201631 Period question, easy

---
+++ src/questions/q_1_1_57/Test.java
@@ -0,0 +1,12 @@
+package questions.q_1_1_57;
+
+import java.time.Period;
+
+public class Test {
+
+    public static void main(String[] args) {
+        Period period = Period.of(0,0,0);
+        System.out.println(period);
+
+    }
+}
