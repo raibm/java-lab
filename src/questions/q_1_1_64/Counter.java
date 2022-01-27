6d13803afe9e84665933003f500ffa6f7a808b47
Rai Bizerra Maciel <45213677+raibm@users.noreply.github.com>
2022-01-26 21:15:02 -0300

    202201262114 Fucking joke

---
+++ src/questions/q_1_1_64/Counter.java
@@ -0,0 +1,14 @@
+package questions.q_1_1_64;
+
+public class Counter {
+    int count;
+
+    Counter(int count){
+        this.count = count;
+    }
+
+    public String toString(){
+        return "jrwjr-"+count;
+    }
+
+}
---
+++ src/questions/q_1_1_64/Test.java
@@ -0,0 +1,22 @@
+package questions.q_1_1_64;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Test {
+
+    public static void main(String[] args) {
+        ArrayList<Counter> original = new ArrayList<>();
+        original.add(new Counter(10));
+
+        ArrayList<Counter> clone = (ArrayList<Counter>) original.clone();
+
+        clone.get(0).count = 5;
+
+
+
+
+
+        System.out.println(original);
+    }
+}
